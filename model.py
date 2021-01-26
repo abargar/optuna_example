d5dc71e05fc2a69bd573fd0db918627d6c23079f
abargar <aliciabargar@gmail.com>
2021-01-25 20:33:54 -0500

    basic scripts

---
+++ model.py
@@ -0,0 +1,98 @@
+import gensim
+from gensim.models import CoherenceModel
+from gensim import corpora
+import optuna
+import pandas as pd
+import logging
+import csv
+from pathlib import Path
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.FileHandler("optuna.log", mode="w"))
+
+token_df = pd.read_parquet("data/tokenized_ingredients.parquet")
+tokens_list = token_df.tokens.values
+corpora_dict = corpora.Dictionary(tokens_list)
+corpora_dict.save("data/tokens.dict")
+corpus = [corpora_dict.doc2bow(tokens) for tokens in tokens_list]
+corpora.MmCorpus.serialize("data/token_corpus.mm", corpus)
+dt_mat = [tf for tf in corpus]
+
+with open("model_results.csv", "w") as f:
+    csvwriter = csv.DictWriter(
+        f, fieldnames=["trialno", "coherence", "ntopics", "alpha", "eta"]
+    )
+    csvwriter.writeheader()
+
+
+def compute_coherence(model, corpus, corpora_dict):
+    coherence_model_lda = CoherenceModel(
+        model=model,
+        texts=corpus,
+        corpus=None,
+        dictionary=corpora_dict,
+        coherence="c_v",
+    )
+    return coherence_model_lda.get_coherence()
+
+
+def get_and_save_top_words(model, out_file):
+    top_words_per_topic = []
+    for t in range(model.num_topics):
+        top_words_per_topic.extend([(t,) + x for x in model.show_topic(t, topn=15)])
+    pd.DataFrame(top_words_per_topic, columns=["topic", "word", "p"]).to_parquet(
+        path=out_file, index=False
+    )
+
+
+def objective(trial):
+    alpha = trial.suggest_uniform("alpha", 0.01, 1)
+    eta = trial.suggest_uniform("eta", 0.01, 1)
+    num_topics = trial.suggest_uniform("num_topics", 10, 50)
+    ideal_score = 0.8
+    model = gensim.models.LdaMulticore(
+        workers=7,
+        corpus=dt_mat,
+        id2word=corpora_dict,
+        num_topics=num_topics,
+        random_state=100,
+        passes=3,
+        alpha=alpha,
+        eta=eta,
+        per_word_topics=True,
+    )
+    print(f"Check the corpus: {len(corpus)}")
+    print(f"Check the dict: {len(corpora_dict)}")
+    print(f"Check the model: {str(model)}")
+    coherence_score = compute_coherence(model, tokens_list, corpora_dict)
+    print(f"Coherence score: {coherence_score}")
+    with open("model_results.csv", "a") as f:
+        csvwriter = csv.DictWriter(
+            f, fieldnames=["trialno", "coherence", "ntopics", "alpha", "eta"]
+        )
+        csvwriter.writerow(
+            {
+                "trialno": trial.number,
+                "coherence": coherence_score,
+                "ntopics": num_topics,
+                "alpha": alpha,
+                "eta": eta,
+            }
+        )
+    if coherence_score >= 0.45:
+        model_path = Path(f"models/trial_{trial.number}")
+        model_path.mkdir(parents=True, exist_ok=True)
+        model.save(str(model_path / f"{trial.number}_lda"))
+        top_words_filename = model_path / f"trialno{trial.number}_top_words.parquet"
+        get_and_save_top_words(model, top_words_filename)
+    coherence_score_diff = abs(ideal_score - coherence_score)
+    return coherence_score_diff
+
+
+study = optuna.create_study()
+study.optimize(objective, n_trials=100)
+Path(f"models").mkdir(exist_ok=True)
+
+logger.info(f"Best trial: {study.best_trial.number}")
+logger.info(f"Best trial info: {study.best_trial}")
---
+++ process_ingredients.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import re
+import string
+from pathlib import Path
+import json
+from time import time
+
+p = re.compile("[A-Za-z]+")
+crap_words = set(
+    [
+        "in",
+        "a",
+        "the",
+        "to",
+        "with",
+        "and",
+        "until",
+        "an",
+        "of",
+        "into",
+        "cup",
+        "pound",
+        "cups",
+        "pounds",
+        "advertisement",
+        "tablespoon",
+        "tablespoons",
+        "teaspoon",
+        "teaspoons",
+        "or",
+        "ground",
+        "ounces",
+        "for",
+        "grams",
+        "g",
+        "fresh",
+        "freshly",
+    ]
+)
+
+
+def pull_from_file(fname):
+    f_recipes = []
+    f = json.load(open(fname, "r"))
+    for rid, recipe in f.items():
+        if (recipe.get("title") is not None) and (
+            recipe.get("ingredients") is not None
+        ):
+            ingredients = " ".join(recipe["ingredients"])
+            f_recipes.append((rid, recipe["title"], ingredients))
+    return f_recipes
+
+
+def make_tokens(text):
+    text = text.lower()
+    only_words = [
+        t.translate(str.maketrans("", "", string.punctuation))
+        for t in text.split()
+        if p.match(t)
+    ]
+    return [word for word in only_words if word not in crap_words]
+
+
+# pull
+files = [f for f in Path("data/raw").iterdir() if not f.name.endswith("zip")]
+all_recipes = []
+for f in files:
+    f_recipes = pull_from_file(f)
+    all_recipes.extend(f_recipes)
+ingredients_df = pd.DataFrame(
+    all_recipes, columns=["id", "title", "ingredients"]
+).set_index("id")
+print(ingredients_df.head())
+ingredients_df.to_csv("all_ingredients.csv")
+
+# tokenize
+ingredients_df["tokens"] = ingredients_df["title"].str.cat(
+    ingredients_df["ingredients"], " "
+)
+ingredients_df["tokens"] = ingredients_df["tokens"].map(make_tokens)
+print(ingredients_df.head())
+ingredients_df.to_parquet("data/tokenized_ingredients.parquet")
