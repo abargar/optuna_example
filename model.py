7eccf163f8bd487de73ee5b807c9c7485959a857
abargar <aliciabargar@gmail.com>
2021-01-31 19:59:56 -0500

    further simplification with readout placed in its own function

--- model.py
+++ model.py
@@ -19,7 +19,7 @@ corpus = [corpora_dict.doc2bow(tokens) for tokens in tokens_list]
 
 with open("model_results.csv", "w") as f:
     csvwriter = csv.DictWriter(
-        f, fieldnames=["trialno", "coherence", "ntopics", "alpha", "eta"]
+        f, fieldnames=["trial", "coherence", "ntopics", "alpha", "eta"]
     )
     csvwriter.writeheader()
 
@@ -35,6 +35,30 @@ def compute_coherence(model, corpus, corpora_dict):
     return coherence_model_lda.get_coherence()
 
 
+def write_model_results(trial, model, coherence_score):
+    params = trial.params
+    trialnum = trial.number
+    with open("model_results.csv", "a") as f:
+        csvwriter = csv.DictWriter(
+            f, fieldnames=["trial", "coherence", "ntopics", "alpha", "eta"]
+        )
+        csvwriter.writerow(
+            {
+                "trial": trialnum,
+                "coherence": coherence_score,
+                "ntopics": params["num_topics"],
+                "alpha": params["alpha"],
+                "eta": params["eta"],
+            }
+        )
+    if coherence_score >= 0.5:
+        model_path = Path(f"models/trial_{trialnum}")
+        model_path.mkdir(parents=True, exist_ok=True)
+        model.save(str(model_path / f"{trialnum}_lda"))
+        top_words_filename = model_path / f"trial{trialnum}_top_words.parquet"
+        get_and_save_top_words(model, top_words_filename)
+
+
 def get_and_save_top_words(model, out_file):
     top_words_per_topic = []
     for t in range(model.num_topics):
@@ -47,13 +71,13 @@ def get_and_save_top_words(model, out_file):
 def objective(trial):
     alpha = trial.suggest_uniform("alpha", 0.01, 1)
     eta = trial.suggest_uniform("eta", 0.01, 1)
-    num_topics = trial.suggest_uniform("num_topics", 10, 50)
+    ntopics = trial.suggest_uniform("num_topics", 10, 50)
     ideal_score = 0.8
     model = gensim.models.LdaMulticore(
         workers=7,
         corpus=corpus,
         id2word=corpora_dict,
-        num_topics=num_topics,
+        num_topics=ntopics,
         random_state=100,
         passes=3,
         alpha=alpha,
@@ -62,25 +86,7 @@ def objective(trial):
     )
     coherence_score = compute_coherence(model, tokens_list, corpora_dict)
     print(f"Trial {trial.number} coherence score: {round(coherence_score,3)}")
-    with open("model_results.csv", "a") as f:
-        csvwriter = csv.DictWriter(
-            f, fieldnames=["trialno", "coherence", "ntopics", "alpha", "eta"]
-        )
-        csvwriter.writerow(
-            {
-                "trialno": trial.number,
-                "coherence": coherence_score,
-                "ntopics": num_topics,
-                "alpha": alpha,
-                "eta": eta,
-            }
-        )
-    if coherence_score >= 0.5:
-        model_path = Path(f"models/trial_{trial.number}")
-        model_path.mkdir(parents=True, exist_ok=True)
-        model.save(str(model_path / f"{trial.number}_lda"))
-        top_words_filename = model_path / f"trialno{trial.number}_top_words.parquet"
-        get_and_save_top_words(model, top_words_filename)
+    write_model_results(trial, model, coherence_score)
     coherence_score_diff = abs(ideal_score - coherence_score)
     return coherence_score_diff
 
